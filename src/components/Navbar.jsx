21fcfe3853b4b6153888b0e8b904c075255dfe33
Mohammad-Ghalib <92501236+Mohammad-Ghalib@users.noreply.github.com>
2023-07-15 18:41:13 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,9 @@ const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
+      <Typography className="logo" variant="h4" fontWeight="bold" sx={{ paddingLeft:"15px", mt: 1.5, color: "#fff"}}>
+          U <span style={{ color: "#FC1503" }}>TUBE</span>
+      </Typography>
     </Link>
     <SearchBar />
   </Stack>
